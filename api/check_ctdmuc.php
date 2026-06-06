89cf5f54e229c108e1f16578fb2d062ad9cc9b95
mylovetron2 <mystore2018page1@gmail.com>
2026-06-06 20:03:18 +0700

    debug: show ctdmuc structure

--- api/check_ctdmuc.php
+++ api/check_ctdmuc.php
@@ -1,18 +1,13 @@
 <?php
 require_once 'config.php';
-// Check bảng bhld_ctdmuc
-$tables = ['bhld_ctdmuc', 'bhld_ct_dmuc', 'bhld_dmuc_ct', 'bhld_ctdm', 'bhld_dmvattu_ct'];
-foreach ($tables as $t) {
-    $r = mysqli_query($conn, "SELECT COUNT(*) as cnt FROM `$t`");
-    if ($r) {
-        $row = mysqli_fetch_assoc($r);
-        echo "$t: {$row['cnt']} rows<br>";
-    } else {
-        echo "$t: NOT FOUND<br>";
-    }
-}
-// Show all bhld_ tables
-$r2 = mysqli_query($conn, "SHOW TABLES LIKE 'bhld_%'");
-echo "<br>All bhld_ tables:<br>";
-while ($row = mysqli_fetch_row($r2)) echo $row[0] . "<br>";
+
+// Cấu trúc bảng bhld_ctdmuc
+echo "<b>DESCRIBE bhld_ctdmuc:</b><br>";
+$r = mysqli_query($conn, "DESCRIBE bhld_ctdmuc");
+while ($row = mysqli_fetch_assoc($r)) echo $row['Field'] . " (" . $row['Type'] . ")<br>";
+
+// 5 dòng đầu
+echo "<br><b>Sample data:</b><br>";
+$r2 = mysqli_query($conn, "SELECT * FROM bhld_ctdmuc LIMIT 5");
+while ($row = mysqli_fetch_assoc($r2)) echo json_encode($row, JSON_UNESCAPED_UNICODE) . "<br>";
 ?>
